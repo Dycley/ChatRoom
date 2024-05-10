65a38b6ce4c1281bbb67cdf399a3074480e74823
dycley <dyk_693@qq.com>
2024-05-10 13:42:48 +0800

    some fixes

--- Client/view/chat_UI.c
+++ Client/view/chat_UI.c
@@ -20,25 +20,25 @@ void Chat_Room_UI() {
     isInRoom = 1;
     print_colored("cyan","输入消息并按回车以发送 菜单选项: /r 返回上一级 /u 上传文件 /d 下载文件 /e 退出\n");
     while (1){
-        scanf("%799s",msg);
-        clear_input_buffer();
-        if(strcmp(msg,"/r")==0){
+        fgets(msg,sizeof(msg),stdin);
+//        clear_input_buffer();
+        if(strcmp(msg,"/r\n")==0){
             Account_Menu_UI();
             return;
-        }else if(strcmp(msg,"/u")==0){
+        }else if(strcmp(msg,"/u\n")==0){
             print_colored("cyan","请输入上传的文件路径: ");
             scanf("%199s",filepath);
             clear_input_buffer();
             if(Chat_Srv_Send_File(filepath) < 0){
                 print_colored("cyan","输入消息并按回车以发送 菜单选项: /r 返回上一级 /u 上传文件 /d 下载文件 /e 退出\n");
             }
-        }else if(strcmp(msg,"/d")==0){
+        }else if(strcmp(msg,"/d\n")==0){
             print_colored("cyan","请输入要下载的文件名: ");
             scanf("%199s",filename);
             clear_input_buffer();
             Chat_Srv_Download(filename);
             print_colored("cyan","输入消息并按回车以发送 菜单选项: /r 返回上一级 /u 上传文件 /d 下载文件 /e 退出\n");
-        }else if(strcmp(msg,"/e")==0){
+        }else if(strcmp(msg,"/e\n")==0){
             return;
         }else{
             clean_last_line();
